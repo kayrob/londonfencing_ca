a2b07b0df8a5a5dec81a9f86c23414250dcfb5aa
Karen Laansoo <karen@res.im>
2012-03-23 22:36:19 -0400

    added images and trillium widget

---
+++ src/LondonFencing/trillium/Widgets/views/trilliumCallout.php
@@ -0,0 +1,5 @@
+<section class="callout" id="trillium">
+    <h2>OTF Grant</h2>
+    <p>The London Fencing Club is proud to be funded by the Ontario Trillium Foundation</p>
+    <p><a href="http://www.trilliumfoundation.org/en/index.asp">Learn more about the Ontario Trillium Foundation</a></p>
+</section>
