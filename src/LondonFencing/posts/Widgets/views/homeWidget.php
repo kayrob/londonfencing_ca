2097cca082b74141ce5afb2d8427801d137cd198
Karen Laansoo <karen@res.im>
2012-05-05 00:06:47 -0400

    display 2 news articles on home page, updated xml file for feeds

--- src/LondonFencing/posts/Widgets/views/homeWidget.php
+++ src/LondonFencing/posts/Widgets/views/homeWidget.php
@@ -1,19 +1,20 @@
 <?php
-$row = array();
+$articles = array();
 if (isset($db) && $this INSTANCEOF Quipp){
     $qry = "SELECT `slug`, `lead_in`, `type`, `title` , UNIX_TIMESTAMP(`displayDate`) as displayDate, `author` FROM `tblNews` WHERE `approvalStatus` = '1' AND `sysStatus` = 'active' AND `sysOpen` = '1' AND `type` IN ('news') AND UNIX_TIMESTAMP(`displayDate`) <= UNIX_TIMESTAMP() ORDER BY `displayDate` DESC, `itemID` DESC LIMIT 0,2 ";
     $res = $db->query($qry);
     if ($res->num_rows > 0){
-        $row[] = $db->fetch_assoc($res);
-        
+        while ($row = $db->fetch_assoc($res)){
+            $articles[] = $row;
+        }
     }
 }
 ?>
 <section class="callout" id="latestNews">
     <h2>Latest News</h2>
 <?php
-    if (!empty($row)){
-        foreach($row as $data){
+    if (!empty($articles)){
+        foreach($articles as $data){
             $end = (strlen(strip_tags(trim($data["lead_in"]))) > 300)?strpos(strip_tags(trim($data["lead_in"]))," ", 295):strlen(strip_tags(trim($data["lead_in"])));
             $lead = substr(strip_tags(trim($data["lead_in"])),0,$end);
             if ($end < strlen(strip_tags(trim($data["lead_in"])))){ $lead .= "&hellip;"; }
