f5ca2ced89cdb913c5370587cfccfbfba2c9d4b8
Karen Laansoo <karenlaansoo@gmail.com>
2013-01-15 00:26:24 -0500

    google analytics

--- themes/LondonFencing/footer.php
+++ themes/LondonFencing/footer.php
@@ -13,11 +13,7 @@
     
     <div class="clearfix"></div>
     
-</div> <!--! end of #container -->
-
-
-
-    
+</div> <!--! end of #container --> 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="/js/jquery-1.6.4.min.js"><\/script>')</script>
   <script src="/js/jquery-ui/jquery-ui-1.8.16.custom.min.js"></script>
@@ -31,15 +27,17 @@
         }
     }
 ?>
-
   <script src="/themes/LondonFencing/site.js"></script>
-
-  <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-  </script>
+  <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37613468-2']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
   <!--[if lt IE 7 ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
