aad0e1e3a360272f5a5e28529de0c5624a52b041
Karen Laansoo <karenlaansoo@gmail.com>
2014-06-18 08:57:39 -0400

    beforeShow for positioning date picker

--- src/LondonFencing/registration/assets/js/registration.js
+++ src/LondonFencing/registration/assets/js/registration.js
@@ -1,10 +1,36 @@
+(function ($) {
+    $.extend($.datepicker, {
+
+        // Reference the orignal function so we can override it and call it later
+        _inlineDatepicker2: $.datepicker._inlineDatepicker,
+
+        // Override the _inlineDatepicker method
+        _inlineDatepicker: function (target, inst) {
+
+            // Call the original
+            this._inlineDatepicker2(target, inst);
+
+            var beforeShow = $.datepicker._get(inst, 'beforeShow');
+
+            if (beforeShow) {
+                beforeShow.apply(target, [target, inst]);
+            }
+        }
+    });
+}(jQuery));
 jQuery(document).ready(function($){
     
         $('#RQvalDATEbirthDate').datepicker({
                 dateFormat : 'yy-mm-dd',
                 changeMonth: true,
                 changeYear: true,
-                yearRange: "-80:+0"
+                yearRange: "-80:+0",
+                beforeShow: function(input){
+                    var top = $("#" + input.id).position().top + 40;
+                    setTimeout(function(){
+                        $('#ui-datepicker-div').css({'top':top});      
+                    },1);
+                }
         });
         
         $('#terms').fancybox();
