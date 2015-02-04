8d62c1d03cf8af3f59679337b987309870cf8aa2
karenlaansoo <karenlaansoo@192.168.1.108>
2015-02-03 22:52:09 -0500

    [foundation report update]

--- src/LondonFencing/reports/assets/foundationReport.php
+++ src/LondonFencing/reports/assets/foundationReport.php
@@ -9,16 +9,20 @@ if (isset($_POST['submitFoundation']) && isset($_POST['foundationStart']) && iss
         $rpts = new RPT\reports($db);
         $members = $rpts->getFoundationsMembers(strtotime($db->escape($_POST['foundationStart'], true)), strtotime($db->escape($_POST['foundationEnd'],true)));
         if (!empty($members)){
-$csv = "CFF Licence,Lastname,Firstname,Email,Gender,Club,Club - 2,Year of Birth,Address,City,Province,Postal Code,Country,Phone,Language,Fencer,Booster,CFF license ( please check -$18. will be added)\n";
+//$csv = "CFF Licence,Lastname,Firstname,Email,Gender,Club,Club - 2,Year of Birth,Address,City,Province,Postal Code,Country,Phone,Language,Fencer,Booster,CFF license ( please check -$18. will be added)\n";
+            $csv = "Firstname,Lastname,Gender,Club,Year of Birth,Membership Level,Address,City,Postal Code,Phone,CFF license (if known),Email\n";
         foreach($members as $mInfo){
                 $address = (!empty($mInfo["address2"]) ? $mInfo['address2']."-".$mInfo['address'] : $mInfo['address']);
                 $postal = str_replace(" ", "", $mInfo['postalCode']);
                 $postal = substr($postal, 0, 3) ." ". substr($postal, 3);
                 $phone = preg_replace("%[^\d]%", "-", $mInfo['phoneNumber']);
-                $csv .= ",\"".$mInfo['lastName']."\",\"".$mInfo['firstName']."\",".$mInfo["email"].",".$mInfo['gender'].",LFC,,".date("Y",$mInfo["birthDate"]).",";
+              /*  $csv .= ",\"".$mInfo['lastName']."\",\"".$mInfo['firstName']."\",".$mInfo["email"].",".$mInfo['gender'].",LFC,,".date("Y",$mInfo["birthDate"]).",";
                 $csv .= "\"".trim($address, "-")."\",";
                 $csv .= "\"".$mInfo['city']."\",".$mInfo['province'].",".$postal.",Canada,".$phone.",";
-                $csv .= ",1,,\n";
+                $csv .= ",1,,\n";*/
+                $csv .= "\"".$mInfo['firstName']."\",\"".$mInfo['lastName']."\",".$mInfo['gender'].",LON,".date("Y",$mInfo["birthDate"]).",";
+                $csv .= "Recreation,\"".trim($address, "-")."\",";
+                $csv .= "\"".$mInfo['city']."\",".$postal.",".$phone.",,".$mInfo["email"]."\n";
             }
 header("Content-type: application/csv; charset=utf-8");
 header("Content-Disposition: attachment; filename=LFC_Foundation_".$_POST['foundationStart']."-".$_POST['foundationEnd'].".csv");
